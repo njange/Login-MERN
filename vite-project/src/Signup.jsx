472a1f959272369cdf5b1eee088574ad1ffb074b
njange <jamesnjange80@gmail.com>
2024-08-06 20:39:04 +0300

    signup userstates

--- vite-project/src/Signup.jsx
+++ vite-project/src/Signup.jsx
@@ -1,70 +1,69 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 
 function Signup() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    
     return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
             <div className="bg-white p-3 rounded w-25">
                 <h2>Register</h2>
                 <form>
                     <div className="mb-3">
-                        <label htmlFor="email">
+                        <label htmlFor="name">
                             <strong>Name</strong>
                         </label>
                         <input
-                        type="text"
-                        placeholder="Enter Name"
-                        autoComplete="off"
-                        name="email"
-                        className="form-control rounded-0"
+                            type="text"
+                            placeholder="Enter Name"
+                            autoComplete="off"
+                            name="name"
+                            className="form-control rounded-0"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
-                        <div className="mb-3">
-                            <label htmlFor="email">
-                                <strong>Email</strong>
-                                </label>
-                                <input
-                                type="email"
-                                placeholder="Enter Email"
-                                autoComplete="off"
-                                name="email"
-                                className="form-control rounded-0"
-                                />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="password">
-                                <strong>Password</strong>
-                                </label>
-                                <input
-                                type="password"
-                                placeholder="Enter Password"
-                                autoComplete="off"
-                                name="password"
-                                className="form-control rounded-0"
-                            />
-                            
-                        </div>
-                        <button type="submit" className="btn btn-success w-100 rounded-0">
-                            Register
-
-                        </button>
-                        <p>Already Have an Account</p>
-                        <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
-                            Login
-                        </Link>
                     </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Email</strong>
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Enter Email"
+                            autoComplete="off"
+                            name="email"
+                            className="form-control rounded-0"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password">
+                            <strong>Password</strong>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            autoComplete="off"
+                            name="password"
+                            className="form-control rounded-0"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        Register
+                    </button>
+                    <p>Already Have an Account</p>
+                    <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                        Login
+                    </button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
 export default Signup;
-
