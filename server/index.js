cf147ecb1e2f38c59519a0458ca656b1631420f0
njange <jamesnjange80@gmail.com>
2024-08-07 15:16:18 +0300

    employee route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {});
 
-app.post('/register', (req, res) => {});
+app.post('/register', (req, res) => {
+    Employee.create(req.body)
+        .then(employees => res.json(employees))
+        .catch(err => res.status(400).json(err));
+});
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
