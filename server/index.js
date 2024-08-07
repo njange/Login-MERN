6220748703f0ded0134b735bb1f46b8d4a8c2914
njange <jamesnjange80@gmail.com>
2024-08-07 15:10:59 +0300

    post request register

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {});
 
-
+app.post('/register', (req, res) => {});
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
