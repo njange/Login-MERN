cc60f5782727f3b1a973cdee1d0787655d6db934
njange <jamesnjange80@gmail.com>
2024-08-07 16:19:56 +0300

    connected to local server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb://127.0.0.1:27017/loginapp")
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
