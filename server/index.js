93aa0bb9488624516ef6112980d8dd0e3d4b4668
njange <jamesnjange80@gmail.com>
2024-08-07 15:02:07 +0300

    server connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI, {});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
