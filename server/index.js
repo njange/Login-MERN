c73935d70b05e33249371aa4291b207eab3e050c
njange <jamesnjange80@gmail.com>
2024-08-07 15:13:54 +0300

    employee model

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const Employee = require('./models/Employee');
 
 const app = express();
 app.use(cors());
