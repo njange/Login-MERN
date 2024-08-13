8cedfc440e332124188b2d4c84db9bdc2464a90b
njange <jamesnjange80@gmail.com>
2024-08-13 20:09:47 +0300

    home route

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
    <Routes>
     <Route path="/register" element={<Signup />} />
     <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
    </Routes>
    </BrowserRouter>
   )
