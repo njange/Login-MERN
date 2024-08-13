02edf464511f796564521f90999459c4b8f9f2fe
njange <jamesnjange80@gmail.com>
2024-08-13 12:45:53 +0300

    navigation fix

--- vite-project/src/Signup.jsx
+++ vite-project/src/Signup.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault()
         axios.post('http://localhost:3001/register', {name, email, password})
-            .then(result => console.log(result))
+            .then(result => {console.log(result)
+                Navigate('/login')
+            })
             .catch(err => console.log(err))
     };
 
@@ -18,7 +22,7 @@ function Signup() {
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
             <div className="bg-white p-3 rounded w-25">
                 <h2>Register</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="name">
                             <strong>Name</strong>
