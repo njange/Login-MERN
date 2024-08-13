95dbd6ab7adf85a02a15506505b71df082fd0ece
njange <jamesnjange80@gmail.com>
2024-08-13 14:51:10 +0300

    user password login

--- server/index.js
+++ server/index.js
@@ -13,6 +13,23 @@ mongoose.connect("mongodb://127.0.0.1:27017/employee")
     .catch(err => console.log(err));
 
 
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  Employee.findOne({ email: email})
+  .then(user => {
+    if(user) {
+      if(user.password === password) {
+        res.json('Login Success');
+      } else {
+        res.json('Password does not match');
+      }
+    } else {
+      res.json('User not registered');
+    }
+  })
+  })
+
+
 app.post('/register', (req, res) => {
     Employee.create(req.body)
         .then(employees => res.json(employees))
