0819db1ccec0eeb20bf0d64cdae8d41aa897fc37
njange <jamesnjange80@gmail.com>
2024-08-13 20:18:13 +0300

    complete login app

---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', padding: '20px' }}>
+      <h1>Home Component</h1>
+      <h2 style={{ color: 'red' }}>You have successfully logged in</h2>
+    </div>
+  );
+}
+
+export default Home;
